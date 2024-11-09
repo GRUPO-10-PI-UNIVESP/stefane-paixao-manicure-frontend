53896a31d6154d713260cacb29253b650983f6bb
Merces-dev <giovanitjs013@gmail.com>
2024-11-09 11:09:20 -0300

    ci: add backend env

--- src/core/configs/axios/index.ts
+++ src/core/configs/axios/index.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    "https://orca-app-8cyky.ondigitalocean.app/stefane-paixao-manicure-backend",
 });
 
 export default axiosInstance;
